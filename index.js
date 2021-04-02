fc5176407836b236cb57c10ae50b3ea53bfeccd6
Dalia Cavazos <dalia.codes@gmail.com>
2021-04-02 15:46:31 -0400

    The Final Countdown

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+function writeCards(names,event) {
+    for (let i = 0; i < writeCards.length; i++) {
+        console.log(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+
+    return writeCards;
+}
+
+writeCards(["Ada", "Brendan", "Ali"], "birthday"); 
+
+function countDown(i) {
+    while (i >= 0) {
+        console.log(i--);
+    }
+}
+
+countDown(10)
